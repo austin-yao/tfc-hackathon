857e4a124670e229d46eb3342f535688f0704baa
austin-yao <austin.y.yao@gmail.com>
2023-02-25 18:26:57 -0500

    remove pullData

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@
 
 const App = () => {
   const x = 3
+
+  const pullData = (event) => {
+    event.preventDefault()
+    // make data base call to grab data with this query
+    // need to re-render page
+  }
+
   return (
     <div className="container">
       <h1>Welcome!</h1>
-      <form onSubmit={pullData}>
+      <form>
         <div className="form-group">
           <label for="Zip_code">Enter Zip Code: </label>
           <input type="text" placeholder="00000" pattern="[0-9]{5,5}" className="form-control" /><br />
